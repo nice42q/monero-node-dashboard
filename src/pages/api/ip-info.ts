29aaa9951be1156f567e870e0fc2446cdbf7f1a9
Luke <17616949+nice42q@users.noreply.github.com>
2026-06-04 19:46:01 +0200

    Add files via upload

--- src/pages/api/ip-info.ts
+++ src/pages/api/ip-info.ts
@@ -1,9 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import geoip from 'geoip-lite';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { ip } = req.query;
   if (!ip || typeof ip !== 'string') return res.status(400).json({ country: null });
+  
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const geoip = require('geoip-lite');
+  
   const geo = geoip.lookup(ip);
   res.status(200).json({ country: geo?.country?.toLowerCase() || null });
 }
